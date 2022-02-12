755e42002364fe9a55072be5542d857c33e99405
tdrevon <ronandrevon@gmail.com>
2022-02-12 11:07:31 +0000

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # edly
+
+pip install flask tarikDrevonUtils tarikED==1.0.7rc0 wheel
+
+ln -s <your_tiff_folder_location> static/tiff
---
+++ static/js/script.js
@@ -0,0 +1,26 @@
+$(document).on('submit','#bloch-form',function(e){
+  e.preventDefault();
+
+  $.ajax({
+    type:'POST',
+    url:'/solve_bloch',
+    data:{
+        keV:$('#keV').val(),
+        // theta:$('#theta').val(),
+        // phi:$('#phi').val(),
+        // Nmax:$('#Nmax').val(),
+        // Smax:$('#Smax').val(),
+        // thick:$('#thick').val(),
+      // csrfmiddlewaretoken:$('input[name=csrfmiddlewaretoken]').val(),
+    },
+    success: function(response){
+       console.log(response);
+       // $("#display").empty();
+       // $("#display").append(response);
+       console.log(response);
+    },
+    error: function(response){
+        alert('An error occured')
+    }
+  });
+});
